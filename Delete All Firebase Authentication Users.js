596695a15ac2863c5d35c356d9b00e05fb52ef8c
Patrick Mulenga <135840104+pashaaa-17@users.noreply.github.com>
2023-11-23 09:55:04 +0200

    Create Delete All Firebase Authentication Users.js

---
+++ Delete All Firebase Authentication Users.js
@@ -0,0 +1,36 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require("/path/to/service/accounts/private/key/json/file");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "/url/to/your/database"
+});
+
+function deleteUser(uid) {
+    admin.auth().deleteUser(uid)
+        .then(function() {
+            console.log('Successfully deleted user', uid);
+        })
+        .catch(function(error) {
+            console.log('Error deleting user:', error);
+        });
+}
+
+function getAllUsers(nextPageToken) {
+    admin.auth().listUsers(100, nextPageToken)
+        .then(function(listUsersResult) {
+            listUsersResult.users.forEach(function(userRecord) {
+                uid = userRecord.toJSON().uid;
+                deleteUser(uid);
+            });
+            if (listUsersResult.pageToken) {
+                getAllUsers(listUsersResult.pageToken);
+            }
+        })
+        .catch(function(error) {
+            console.log('Error listing users:', error);
+        });
+}
+
+getAllUsers();
